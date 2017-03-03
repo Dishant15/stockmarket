f32b65c9fc9077197762d16a417d63cc9508e4ea
Dishant Chavda <dishantchavda@gmail.com>
2017-03-03 22:38:44 +0530

    echo server ready and tested

--- stockmarket/routing.py
+++ stockmarket/routing.py
@@ -1,4 +1,6 @@
 from channels.routing import route
+from market.consumers import ws_message
+
 channel_routing = [
-    route("http.request", "market.consumers.http_consumer"),
+	route("websocket.receive", ws_message),
 ]
