c3263455dbc2ce6e63d79a759c79bcc2ddbeb1ac
Dishant Chavda <dishantchavda@gmail.com>
2017-03-09 11:31:24 +0530

    solving settings prob

--- stockmarket/settings_live.py
+++ stockmarket/settings_live.py
@@ -14,7 +14,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "asgi_redis.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": ['redis://localhost:6379'],
         },
         "ROUTING": "stockmarket.routing.channel_routing",
     },
--- stockmarket/settings_local.py
+++ stockmarket/settings_local.py
@@ -1,3 +1,5 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
